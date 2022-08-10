36a57ea55e643c34042c88feee39a8a3f9fdf994
Siva Prakash <prakashdeveloper@outlook.in>
2022-08-10 15:52:28 +0530

    Added main.py, readme.md & markdown folder

---
+++ main.py
@@ -0,0 +1,61 @@
+from joblib import load
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()  # instance of FastAPI class
+
+# mount static folder files to /static route
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# loads the ML models
+classifier = load(open("models/spamClassifier.pkl", "rb"))
+transform = load(open("models/transform.pkl", "rb"))
+
+# sets the templates folder for the app
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    """
+    Function to render base.html at route '/' as a get request
+    Args:
+        request (Request): request in path operation that will return a template
+    Returns:
+        TemplateResponse: render base.html
+    """
+    return templates.TemplateResponse("base.html", {"request": request})
+
+
+@app.post("/predict", response_class=HTMLResponse)
+async def predict(
+    request: Request,
+    message: str = Form(...),
+):
+    """
+    Function to predict spam status
+    and shows the result by rendering base.html at route '/predict'
+    Args:
+        request (Request): request in path operation that will return a template
+        message (str): message text
+    Returns:
+        Template: render base.html
+    """
+    data = [message]  # str -> [str]
+    vect = transform.transform(
+        data
+    ).toarray()  # convert a collection of text documents to a matrix of token counts
+    prediction = classifier.predict(
+        vect
+    )  # prediction using multinomial naive bayes model
+    print(prediction)
+    output = (
+        "Gotcha! This is a SPAM message."
+        if prediction == 1
+        else "Great! This is NOT a spam message."
+    )
+    return templates.TemplateResponse(
+        "base.html", context={"request": request, "prediction": output}
+    )
---
+++ readme.md
@@ -0,0 +1,46 @@
+# SMS Spam Classifier
+![made-with-python](https://img.shields.io/badge/Made%20with-Python-0078D4.svg)
+![html5](https://img.shields.io/badge/HTML5-E34F26?logo=html5&logoColor=white)
+![css3](https://img.shields.io/badge/CSS3-1572B6?logo=css3&logoColor=white)
+![pandas](https://img.shields.io/badge/Pandas-2C2D72?logo=pandas&logoColor=white)
+![scikit-learn](https://img.shields.io/badge/scikit_learn-0078D4?logo=scikit-learn&logoColor=white)
+![fastapi](https://img.shields.io/badge/fastapi-109989?logo=FASTAPI&logoColor=white)
+![heroku](https://img.shields.io/badge/Heroku-430098?logo=heroku&logoColor=white)
+![vscode](https://img.shields.io/badge/Visual_Studio_Code-0078D4?logo=visual%20studio%20code&logoColor=white)
+
+SMS Spam Classifier app is used to predict if the given input message is spam or not. The app created using python's scikit-learn, fastapi, pandas, nltk and joblib packages.
+
+## Installation
+Open Anaconda prompt and create new environment
+```
+conda create -n your_env_name python=3.10.4
+```
+Then Activate the newly created environment
+```
+conda activate your_env_name
+```
+Clone the repository using `git`
+```
+git clone https://github.com/Prakashdeveloper03/SMS-Spam-Classifier.git
+```
+Change to the cloned directory
+```
+cd <directory_name>
+```
+To install all requirement packages for the app
+```
+pip install -r requirements.txt
+```
+Then, Run the app
+```
+uvicorn main:app --reload
+```
+## 📷 Screenshots
+### Spam Result
+![spam_image](markdown/spam.png)
+### Not Spam Result
+![not_spam_image](markdown/noSpam.png)
+### Swagger UI
+![swagger_image](markdown/swagger.png)
+### Redoc UI
+![redoc_image](markdown/redoc.png)
